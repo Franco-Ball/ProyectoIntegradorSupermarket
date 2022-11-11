4beeee0c8a06a7bdc70ee013324c69dc461315c2
Santiago Carranza <santiagocarranzazinny@gmail.com>
2022-11-11 08:58:51 -0300

    Addition of models in admin

--- confApp/admin.py
+++ confApp/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from .models import Turnos
+from .models import Direcciones
+from .models import Articulos
+from .models import detalleVentas
+from .models import Ventas
+from .models import Asignaciones
+from .models import Cajas
+from .models import Sucursal
+from .models import Cajero
+
+
+
+
 # Register your models here.
+admin.site.register(Turnos)
+admin.site.register(Direcciones)
+admin.site.register(Articulos)
+admin.site.register(detalleVentas)
+admin.site.register(Ventas)
+admin.site.register(Asignaciones)
+admin.site.register(Cajas)
+admin.site.register(Sucursal)
+admin.site.register(Cajero)
