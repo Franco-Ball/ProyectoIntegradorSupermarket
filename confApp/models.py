467ad8c56318908d4bcf901211a1cd2343cd108f
Santiago Carranza <santiagocarranzazinny@gmail.com>
2022-11-11 08:55:19 -0300

    Upgrade in models.py

--- confApp/models.py
+++ confApp/models.py
@@ -2,55 +2,80 @@ from django.db import models
 
 class Turnos(models.Models):
     detalle = models.CharField(max_length=30)
-    horaInicio = models.IntegerField()
-    horaFin = models.IntegerField()
+    horaInicio = models.IntegerField(max_length=2, default=8)
+    horaFin = models.IntegerField(max_length=2, default=20)
 
 class Direcciones(models.Models):
-    calle = models.CharField(30)
-    altura = models.IntegerField()
-    barrio = models.CharField(30)
-    provincia = models.CharField(30)
+    calle = models.CharField(max_length=30, default="Calle")
+    altura = models.IntegerField(max_length=5, default=12345)
+    barrio = models.CharField(max_length=70, default="Barrio")
+    provincia = models.CharField(max_length=30, default="Provincia")
 
 class Articulos(models.Models):
-    codigo = models.IntegerField()
-    stock = models.IntegerField()
-    nombre = models.CharField(30)
-    descripcion = models.CharField(30)
-    precio = models.IntegerField()
+    codigo = models.IntegerField(max_length=30, default=123456)
+    stock = models.IntegerField(max_length=30, default=1)
+    nombre = models.CharField(max_length=50, default="Articulo")
+    descripcion = models.CharField(max_length=200, default="Descripcion")
+    precio = models.IntegerField(max_length=30, default=100)
+    
+class detalleVentas(models.Models):
+    numVenta = models.IntegerField(max_length=30, default=1234)
+    numArticulo = models.IntegerField(max_length=30, default=1234)
+    precioUnidad = models.IntegerField(max_length=30, default=1234)
+    cantidad = models.IntegerField(max_length=30, default=1234)
 
 class Ventas(models.Models):
-    fecha = models.DateField()
-    listaDetalleVenta = models.() ###
-	
-class detalleVentas(models.Models):
-    numVenta = models.IntegerField()
-    numArticulo = models.IntegerField()
-    precioUnidad = models.IntegerField()
-    cantidad = models.IntegerField()
+    fecha = models.DateField(default= "2022-09-06")
+    listaDetalleVenta = models.ForeignKey(
+        detalleVentas,
+        on_delete=models.CASCADE, default=1
+    )
 	
 class Asignaciones(models.Models):
-    fechaInicio = models.DateField()
-    fechaFin = models.DateField()
-    listaDeVenta = models.() ###
+    fechaInicio = models.DateField(default= "2022-09-06")
+    fechaFin = models.DateField(default= "2022-09-06")
+    listaDeVenta = models.ForeignKey(
+        Ventas,
+        on_delete=models.CASCADE, default=1
+    )
 	
 class Cajas(models.Models):
-    estado = models.BooleanField()
-    listaDeAsignacion = models.() ###
+    estado = models.BooleanField(default=True)
+    listaDeAsignacion = models.ForeignKey(
+        Asignaciones,
+        on_delete=models.CASCADE, default=1
+    )
 
 class Sucursal(models.Models):
-    telefono = models.CharField(30)
-    direccion = models.CharField(30)
-    listaDeCaja = models.() ###
+    telefono = models.CharField(max_length = 20, default="3513897259")
+    direccion = models.CharField(max_length = 50, default="Direccion")
+    listaDeCaja = models.ForeignKey(
+        Cajas,
+        on_delete=models.CASCADE, default=1
+    )
 
 class Cajero(models.Models):
-    dni = models.CharField(30)
-    nombre = models.CharField(30)
-    apellido = models.CharField(30)
-    nacimiento = models.DateField()
-    direccion = models.() ###
-    telefono = models.CharField(30)
-    ingresoEmpresa = models.DateField()
-    email = models.CharField(50)
-    listaAsignacion = models.Asignacion()
-    turnoTrabajo = models.() ###
-    sucursal = models.() ###
+    dni = models.CharField(max_length = 8, default="46037183")
+    nombre = models.CharField(max_length = 30, default="Nombre")
+    apellido = models.CharField(max_length = 30, default="Apellido")
+    nacimiento = models.DateField(default= "2022-09-06")
+    direccion = models.ForeignKey(
+        Direcciones,
+        on_delete=models.CASCADE, default=1
+    )
+
+    telefono = models.CharField(max_length = 20, default="3513897259")
+    ingresoEmpresa = models.DateField(default= "2022-09-06")
+    email = models.CharField(default="mailfalso@gmail.com")
+    listaAsignacion = models.ForeignKey(
+        Asignaciones,
+        on_delete=models.CASCADE, default=1
+    )
+    turnoTrabajo = models.ForeignKey(
+        Turnos,
+        on_delete=models.CASCADE, default=1
+    )
+    sucursal = models.ForeignKey(
+        Sucursal,
+        on_delete=models.CASCADE, default=1
+    )
