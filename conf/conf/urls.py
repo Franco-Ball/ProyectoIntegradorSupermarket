971674f0e28006e2dc1b8bddf046432837d58d7a
Santiago Carranza <santiagocarranzazinny@gmail.com>
2022-11-17 12:01:04 -0300

    Real output with base.html

--- conf/conf/urls.py
+++ conf/conf/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+
+from confApp.views import(
+    home_screen_view
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_screen_view, name='home'),
 ]
