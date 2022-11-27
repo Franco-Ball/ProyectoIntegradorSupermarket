ff26983c8eac3e312e6788697e7cf1caa276a7c4
Santiago Carranza <santiagocarranzazinny@gmail.com>
2022-11-27 15:13:45 -0300

    changes in principal and added mathfilters

--- conf/conf/settings.py
+++ conf/conf/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'confApp'
+    'confApp',
+    'mathfilters'
 ]
 
 MIDDLEWARE = [
